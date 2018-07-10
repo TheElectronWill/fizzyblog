b2e8f92b9c56f97306d56698f43c591e065289d7
Guillaume R <theelectronwill@gmail.com>
2018-07-10 13:11:47 +0200

    Add class support to genhtml

--- fizzyblog/genhtml.py
+++ fizzyblog/genhtml.py
@@ -1,18 +1,18 @@
 import collections
 
 
-def ul(l: list):
-	return __list_html(l, "ul")
+def ul(l: list, ul_class="", li_class=""):
+	return __list_html(l, "ul", ul_class, li_class)
 
 
-def ol(l: list):
-	return __list_html(l, "ol")
+def ol(l: list, ul_class="", li_class=""):
+	return __list_html(l, "ol", ul_class, li_class)
 
 
-def __list_html(l, ltype):
-	s = [f"<{ltype}>"]
+def __list_html(l, ltype, ul_class, li_class):
+	s = [f"<{ltype} class=\"{ul_class}\">"]
 	for elem in l:
-		s += f"<li>{__list_str(elem, ltype)}</li>"
+		s += f"<li class=\"{li_class}\">{__list_str(elem, ltype)}</li>"
 	s += f"</{ltype}>"
 	return "".join(s)
 
@@ -24,9 +24,9 @@ def __list_str(elem, ltype):
 		return str(elem)
 
 
-def img(url, alt):
-	return f"<img src=\"{url}\" alt=\"{alt}\">"
+def img(url, alt, html_class=""):
+	return f"<img class=\"{html_class}\" src=\"{url}\" alt=\"{alt}\">"
 
 
-def link(url, text):
-	return f"<a href=\"{url}\">{text}</a>"
+def link(url, text, html_class=""):
+	return f"<a class=\"{html_class}\" href=\"{url}\">{text}</a>"
