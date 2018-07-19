449015755b4294cf8e0799718d4ce329a25d58a7
Guillaume R <theelectronwill@gmail.com>
2018-07-19 17:01:17 +0200

    Fix braces_replace and support markdown code blocks and inline code

--- fizzyblog/utils.py
+++ fizzyblog/utils.py
@@ -52,10 +52,11 @@ def getlist(dict, key):
 		dict[key] = v
 	return v
 
-__types = "$@§"
 
+def braces_replace(data: str, replace_function, let_md_code=False, let_deeply=False) -> str:
+	to_eval = "$@§!"
+	in_md_code = None
 
-def braces_replace(data: str, replacer) -> str:
 	res = []
 	content = []
 	count = 0
@@ -64,28 +65,69 @@ def braces_replace(data: str, replacer) -> str:
 	while i < limit:
 		ch = data[i]
 		inext = i + 1
+		inext2 = i + 2
 		cnext = data[inext] if inext < limit else None
-		if ch == "\\" and count == 0 and (cnext in __types):
-			i += 2
-		if (ch in __types) and cnext == "{":
-			count += 1
-			i += 2
-			content.append(ch)
-			content.append(cnext)
-		else:
+		cnext2 = data[inext2] if inext2 < limit else None
+
+		if let_md_code and (count == 0 or let_deeply):
+			if ch == cnext == cnext2 == "`":
+				if in_md_code == "```":
+					in_md_code = None
+				elif in_md_code is None:
+					in_md_code = "```"
+			elif ch == "`":
+				if in_md_code == "`":
+					in_md_code = None
+				elif in_md_code is None:
+					in_md_code = "`"
+
+		if in_md_code is not None:
+			res.append(ch)
 			i += 1
-			if count > 0:
+		else:
+			if ch == "\\" and count == 0 and (cnext in to_eval) and cnext2 == "{":
+				i += 2
+				res.append(cnext)
+			elif (ch in to_eval) and cnext == "{":
+				count += 1
+				i += 2
 				content.append(ch)
-				if ch == "}":
-					count -= 1
-					if count == 0:
-						res.append(replacer("".join(content)))
-						content.clear()
+				content.append(cnext)
 			else:
-				res.append(ch)
+				i += 1
+				if count > 0:
+					content.append(ch)
+					if ch == "{":
+						count += 1
+					if ch == "}":
+						count -= 1
+						if count == 0:
+							res.append(replace_function("".join(content)))
+							content.clear()
+				else:
+					res.append(ch)
+
 	return "".join(res)
 
 if __name__ == '__main__':
-	data = "${in ${inner} @{a:b:c}} @{a${}} }{}{}{}${@{${@{}@{}@{}}}}{}}${ø]]@@@@{]{}{{]}\n"*100
-	r = braces_replace(data, lambda x: "'" + str(x)[2:-1] + "'")
+	data = "${in ${inner} @{a:b:c}} @{a${}} }{}{}{}${@{${@{}@{}@{}}}}{}}${ø]]@@@@{]{}{{]}\n"
+	r = braces_replace(data, lambda x: "<" + str(x)[2:-1] + ">")
+	print(r)
+
+	data = "!{\nstring = f'A string with {variables} inside.'\nstring2 = f'{a}{2}{\"${!{{{_@{}_}}}!}\"}'\n}"
+	r = braces_replace(data, lambda x: "<" + str(x)[2:-1] + ">")
+	print(r)
+
+	data = """Here is a block of markdown code:
+						```java
+						public class MyClass {
+							private final String test = "${should be included as it is, it isn't an expression}"
+						}
+						```
+						- These should be evaluated: ${expr$} @{expr@} !{expr!}
+						- Here is some inline code: `${this isn't an expression either} @{!{}}`
+						- These should be evaluated: ${expr$} @{expr@} !{expr!}
+						- Escapement: blabla \\${escaped} \\ \\! \\{!{not escaped\\}\\}
+					"""
+	r = braces_replace(data, lambda x: "<" + str(x)[2:-1] + ">", True)
 	print(r)
